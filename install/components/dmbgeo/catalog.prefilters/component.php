8111a8cf5da52451aede154dd7bd8d477cc313aa
Davit Mikeladze <dmbgeo@gmail.com>
2020-10-01 16:03:04 +0400

    Update component.php

--- install/components/dmbgeo/catalog.prefilters/component.php
+++ install/components/dmbgeo/catalog.prefilters/component.php
@@ -26,22 +26,47 @@ if (CModule::IncludeModule($module_id)) {
         if ($arParams["ACTION_VARIABLE"] == '' || !preg_match("/^[A-Za-z_][A-Za-z01-9_]*$/", $arParams["ACTION_VARIABLE"])) {
             $arParams["ACTION_VARIABLE"] = "action";
         }
+        if(empty($arParams['ELEMENT_SORT_FIELD'])){
+            $arParams['ELEMENT_SORT_FIELD']='SORT';
+        }
 
-        $arDefaultUrlTemplates404 = array(
-            "element" => "#SECTION_ID#/#ELEMENT_ID#/",
-        );
+        if(empty($arParams['ELEMENT_SORT_ORDER'])){
+            $arParams['ELEMENT_SORT_ORDER']='ASC';
+        }
+        if(empty($arParams['ELEMENT_SORT_FIELD2'])){
+            $arParams['ELEMENT_SORT_FIELD2']='NAME';
+        }
 
-        $arDefaultVariableAliases404 = array();
+        if(empty($arParams['ELEMENT_SORT_ORDER2'])){
+            $arParams['ELEMENT_SORT_ORDER2']='ASC';
+        }
+        
+        $arOrder = array($arParams['ELEMENT_SORT_FIELD'] => $arParams['ELEMENT_SORT_ORDER'],$arParams['ELEMENT_SORT_FIELD2'] => $arParams['ELEMENT_SORT_ORDER2']);
+       
+$arParams['ACTION_VARIABLE'] = (isset($arParams['ACTION_VARIABLE']) ? trim($arParams['ACTION_VARIABLE']) : 'action');
+if ($arParams["ACTION_VARIABLE"] == '' || !preg_match("/^[A-Za-z_][A-Za-z01-9_]*$/", $arParams["ACTION_VARIABLE"]))
+	$arParams["ACTION_VARIABLE"] = "action";
 
-        $arDefaultVariableAliases = array();
+$smartBase = ($arParams["SEF_URL_TEMPLATES"]["section"]? $arParams["SEF_URL_TEMPLATES"]["section"]: "#SECTION_ID#/");
+$arDefaultUrlTemplates404 = array(
+	"sections" => "",
+	"section" => "#SECTION_ID#/",
+	"element" => "#SECTION_ID#/#ELEMENT_ID#/",
+	"compare" => "compare.php?action=COMPARE",
+	"smart_filter" => $smartBase."filter/#SMART_FILTER_PATH#/apply/"
+);
 
-        $arComponentVariables = array(
-            "SECTION_ID",
-            "SECTION_CODE",
-            "ELEMENT_ID",
-            "ELEMENT_CODE",
-            "action",
-        );
+$arDefaultVariableAliases404 = array();
+
+$arDefaultVariableAliases = array();
+
+$arComponentVariables = array(
+	"SECTION_ID",
+	"SECTION_CODE",
+	"ELEMENT_ID",
+	"ELEMENT_CODE",
+	"action",
+);
 
         if ($arParams["SEF_MODE"] == "Y") {
             $arVariables = array();
@@ -50,7 +75,8 @@ if (CModule::IncludeModule($module_id)) {
 
             if (\Bitrix\Main\Loader::includeModule('iblock')) {
                 $engine->addGreedyPart("#SECTION_CODE_PATH#");
-                $engine->setResolveCallback(array("CIBlockFindTools", "resolveComponentEngine"));
+		        $engine->addGreedyPart("#SMART_FILTER_PATH#");
+		        $engine->setResolveCallback(array("CIBlockFindTools", "resolveComponentEngine"));
             }
             $arUrlTemplates = CComponentEngine::makeComponentUrlTemplates($arDefaultUrlTemplates404, $arParams["SEF_URL_TEMPLATES"]);
             $arVariableAliases = CComponentEngine::makeComponentVariableAliases($arDefaultVariableAliases404, $arParams["VARIABLE_ALIASES"]);
@@ -60,7 +86,7 @@ if (CModule::IncludeModule($module_id)) {
                 $arUrlTemplates,
                 $arVariables
             );
-
+            // var_debug($componentPage,$arUrlTemplates,$arVariables,$arDefaultUrlTemplates404);
             CComponentEngine::initComponentVariables($componentPage, $arComponentVariables, $arVariableAliases, $arVariables);
 
             $arResult = array(
@@ -69,68 +95,62 @@ if (CModule::IncludeModule($module_id)) {
                 "VARIABLES" => $arVariables,
                 "ALIASES" => $arVariableAliases,
             );
-
-        
-
+            
             if (isset($arVariables['ELEMENT_CODE'])) {
                 $ELEMENT_VAR_VALUE = $arVariables['ELEMENT_CODE'];
                 $ELEMENT_VAR = "CODE";
             } elseif (isset($arVariables['ELEMENT_ID'])) {
                 $ELEMENT_VAR_VALUE = $arVariables['ELEMENT_ID'];
                 $ELEMENT_VAR = "ID";
+            } else {
+                $ELEMENT_VAR = null;
+                $ELEMENT_VAR_VALUE = null;
             }
-            else{
-                $ELEMENT_VAR=null;
-                $ELEMENT_VAR_VALUE=null;
-            }
-
+            
             if (isset($arVariables['SECTION_CODE'])) {
                 $SECTION_VAR_VALUE = $arVariables['SECTION_CODE'];
                 $SECTION_VAR = "SECTION_CODE";
-            }elseif (isset($arVariables['SECTION_ID'])) {
+            } elseif (isset($arVariables['SECTION_ID'])) {
                 $SECTION_VAR_VALUE = $arVariables['SECTION_ID'];
                 $SECTION_VAR = "SECTION_ID";
-            }
-            elseif (isset($arVariables['SECTION_CODE_PATH'])) {
-                $SECTION_VAR_VALUE = array_pop(explode("/",$arVariables['SECTION_CODE_PATH']));
+            } elseif (isset($arVariables['SECTION_CODE_PATH'])) {
+                $SECTION_VAR_VALUE = array_pop(explode("/", $arVariables['SECTION_CODE_PATH']));
                 $SECTION_VAR = "SECTION_CODE";
+            } else {
+                $SECTION_VAR = null;
+                $SECTION_VAR_VALUE = null;
             }
-            else{
-                $SECTION_VAR=null;
-                $SECTION_VAR_VALUE=null;
-            }
-
-           
+            
             if (isset($arVariables['ELEMENT_CODE']) || isset($arVariables['ELEMENT_ID'])) {
                 $arFilter = array(
                     "ACTIVE" => "Y",
                     'IBLOCK_ID' => $arParams["IBLOCK_ID"],
-                    'INCLUDE_SUBSECTIONS' => $arParams["SECTION_ID"]
+                    'INCLUDE_SUBSECTIONS' => $arParams["SECTION_ID"],
                 );
 
-                if($SECTION_VAR){
-                    $arFilter[$SECTION_VAR]=$SECTION_VAR_VALUE;
+                if ($SECTION_VAR) {
+                    $arFilter[$SECTION_VAR] = $SECTION_VAR_VALUE;
                 }
 
-                if($ELEMENT_VAR){
-                    $arFilter[$ELEMENT_VAR]=$ELEMENT_VAR_VALUE;
+                if ($ELEMENT_VAR) {
+                    $arFilter[$ELEMENT_VAR] = $ELEMENT_VAR_VALUE;
                 }
-
                 $rsElement = \CIBlockElement::GetList(
-                    $arOrder = array("SORT" => "ASC"),
-                   $arFilter
+                    $arOrder,
+                    $arFilter
                 );
                 if ($arElement = $rsElement->GetNextElement()) {
                     $arResult['ELEMENT'] = $arElement->GetFields();
                     $arResult['ELEMENT']['PROPERTIES'] = $arElement->GetProperties();
                     $arSection = \CIBlockSection::GetByID($ITEM['IBLOCK_SECTION_ID'])->fetch();
-                    if(is_array($arSection)) {
-                        $ITEM['SECTION']['SECTION_ID']=$arSection['ID'];
-                        $ITEM['SECTION']['SECTION_CODE']=$arSection['CODE'];
+                    if (is_array($arSection)) {
+                        $ITEM['SECTION']['SECTION_ID'] = $arSection['ID'];
+                        $ITEM['SECTION']['SECTION_CODE'] = $arSection['CODE'];
                     }
 
                     // $APPLICATION->AddChainItem($$arResult['ELEMENT']['NAME']);
-                    $newUrl = str_replace(array("http://www." . $_SERVER['SERVER_NAME'], "https://www." . $_SERVER['SERVER_NAME'], "http://" . $_SERVER['SERVER_NAME'], "https://" . $_SERVER['SERVER_NAME']), "", $arResult['ELEMENT']['PROPERTIES']['FILTER_URL']['~VALUE'] ?? "");
+                    $newUrl = explode(str_replace("www.","",$_SERVER['SERVER_NAME']), $arResult['ELEMENT']['PROPERTIES']['FILTER_URL']['~VALUE'] ?? "");
+                    $newUrl=$newUrl[1]??$newUrl[0];
                     if ($newUrl !== "") {
                         \Prefilters::setNewUrl($newUrl);
                     }
@@ -140,60 +160,70 @@ if (CModule::IncludeModule($module_id)) {
                     }
                 }
             }
-           
+
             if ($arParams['RENDER_TEMPLATE'] == "Y") {
                 $arFilter = array(
                     "ACTIVE" => "Y",
                     'IBLOCK_ID' => $arParams["IBLOCK_ID"],
-                    'INCLUDE_SUBSECTIONS' => $arParams["SECTION_ID"]
+                    'INCLUDE_SUBSECTIONS' => $arParams["SECTION_ID"],
                 );
 
-                if($SECTION_VAR){
-                    $arFilter[$SECTION_VAR]=$SECTION_VAR_VALUE;
-                }
+                if ($SECTION_VAR) {
+                    $arFilter[$SECTION_VAR] = $SECTION_VAR_VALUE;
 
-                $rsElement = \CIBlockElement::GetList(
-                    $arOrder = array($arParams['ELEMENT_SORT_FIELD'] => $arParams['ELEMENT_SORT_ORDER'], $arParams['ELEMENT_SORT_FIELD2'] => $arParams['ELEMENT_SORT_ORDER2']),
-                   $arFilter
-                );
-            
-                $SELECT_ID = $arResult['ELEMENT']['ID'] ?? "0";
-                $ELEMENT_PATH=($arParams["SEF_FOLDER"]??"").($arParams['SEF_URL_TEMPLATES']['element']??"");
-                $SECTION_PATH=($arParams["SEF_FOLDER"]??"").($arParams['SEF_URL_TEMPLATES']['section']??"");
-                $rsElement->SetUrlTemplates($ELEMENT_PATH,$SECTION_PATH);
-                while ($arElement = $rsElement->GetNext()) {
-               
-                    $ITEM = $arElement;
-            
-                    if ($ITEM['ID'] == $SELECT_ID) {
-                        $ITEM['SELECTED'] = true;
-                    } else {
-                        $ITEM['SELECTED'] = false;
+                    $rsElement = \CIBlockElement::GetList(
+                        $arOrder,
+                        $arFilter
+                    );
+
+                    $SELECT_ID = $arResult['ELEMENT']['ID'] ?? "0";
+                    $ELEMENT_PATH = ($arParams["SEF_FOLDER"] ?? "") . ($arParams['SEF_URL_TEMPLATES']['element'] ?? "");
+                    $SECTION_PATH = ($arParams["SEF_FOLDER"] ?? "") . ($arParams['SEF_URL_TEMPLATES']['section'] ?? "");
+                    $rsElement->SetUrlTemplates($ELEMENT_PATH, $SECTION_PATH);
+                    while ($arElement = $rsElement->GetNext()) {
+
+                        $ITEM = $arElement;
+
+                        if ($ITEM['ID'] == $SELECT_ID) {
+                            $ipropValues = new \Bitrix\Iblock\InheritedProperty\ElementValues($ITEM["IBLOCK_ID"], $ITEM["ID"]); 
+                            $ITEM["IPROPERTY_VALUES"] = $ipropValues->getValues();
+                            $GLOBALS['PREDFILTER_SEO']=$ITEM["IPROPERTY_VALUES"];
+                            if(!empty($ITEM["IPROPERTY_VALUES"]['ELEMENT_META_TITLE'])){
+                                $APPLICATION->SetPageProperty("title",  $ITEM["IPROPERTY_VALUES"]['ELEMENT_META_TITLE']);
+                            }
+                            if(!empty($ITEM["IPROPERTY_VALUES"]['ELEMENT_META_KEYWORDS'])){
+                                $APPLICATION->SetPageProperty("keywords",  $ITEM["IPROPERTY_VALUES"]['ELEMENT_META_KEYWORDS']);
+                            }
+                            if(!empty($ITEM["IPROPERTY_VALUES"]['ELEMENT_META_DESCRIPTION'])){
+                                $APPLICATION->SetPageProperty("description",  $ITEM["IPROPERTY_VALUES"]['ELEMENT_META_DESCRIPTION']);
+                            }
+
+                            $ITEM['SELECTED'] = true;
+                        } else {
+                            $ITEM['SELECTED'] = false;
+                        }
+
+                        if ($ITEM['PREVIEW_PICTURE']) {
+                            $ITEM['PREVIEW_PICTURE'] = \CFile::GetFileArray($ITEM['PREVIEW_PICTURE']);
+                        }
+                        if ($ITEM['DETAIL_PICTURE']) {
+                            $ITEM['DETAIL_PICTURE'] = \CFile::GetFileArray($ITEM['DETAIL_PICTURE']);
+                        }
+
+                        $arResult['ITEMS'][] = $ITEM;
                     }
-                    
-                    if ($ITEM['PREVIEW_PICTURE']) {
-                        $ITEM['PREVIEW_PICTURE'] = \CFile::GetFileArray($ITEM['PREVIEW_PICTURE']);
+
+                    if (!empty($arResult['ITEMS'])) {
+                        $this->IncludeComponentTemplate();
                     }
-                    if ($ITEM['DETAIL_PICTURE']) {
-                        $ITEM['DETAIL_PICTURE'] = \CFile::GetFileArray($ITEM['DETAIL_PICTURE']);
-                    }
-                   
-                    $arResult['ITEMS'][] = $ITEM;
-                }
-              
-                if (!empty($arResult['ITEMS'])) {
-                    $this->IncludeComponentTemplate();
                 }
             }
+        } else {
+            ShowError(Loc::getMessage('ERROR_PREFILTERS_SEF_MODE'));
         }
-        else{
-          ShowError(Loc::getMessage('ERROR_PREFILTERS_SEF_MODE'));
-        }
+    } else {
+        ShowError(Loc::getMessage('ERROR_PREFILTERS_MODUL_STATUS'));
     }
-    else{
-      ShowError(Loc::getMessage('ERROR_PREFILTERS_MODUL_STATUS'));
-    }
-}
-else{
-  ShowError(Loc::getMessage('ERROR_PREFILTERS_MODUL_INSTALL'));
+} else {
+    ShowError(Loc::getMessage('ERROR_PREFILTERS_MODUL_INSTALL'));
 }
